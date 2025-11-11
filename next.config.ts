a9bf9a637dbdff4b4bab9d85370e5889db3ddd13
refrain62 <nao_h13@hotmail.com>
2025-11-12 00:40:28 +0900

    キャッシュコンポーネントを有効

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // キャッシコンポーネントを有効にする設定
+  cacheComponents: true,
 };
 
 export default nextConfig;
