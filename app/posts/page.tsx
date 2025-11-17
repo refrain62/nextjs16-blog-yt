0afb66e37256e7da2cdd9ec93eca4ba5538bb999
refrain62 <nao_h13@hotmail.com>
2025-11-17 22:24:47 +0900

    getPosts()の戻りの型を設定

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -26,7 +26,7 @@ const japaneseConotext = [
 ];
 
 
-async function getPosts(){
+async function getPosts(): Promise<Post[]> {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts = await res.json();
   return posts;
